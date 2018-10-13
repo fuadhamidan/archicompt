9a629f34678b30705ff121b7f0af413bcb1a03c7
Fuad Hamidan <fuadhamidan@gmail.com>
2018-10-13 18:03:34 +0700

    add database

---
+++ app/src/main/java/in/jejak/android/data/database/AppDatabase.kt
@@ -0,0 +1,24 @@
+package `in`.jejak.android.data.database
+
+import android.arch.persistence.room.Database
+import android.arch.persistence.room.Room
+import android.arch.persistence.room.RoomDatabase
+import android.content.Context
+
+
+/**
+ * Created by fuado on 2018/10/13.
+ * Email    : fuadhamidan@gmail.com
+ * ---
+ * Description:
+ * ...
+ */
+
+@Database(entities = [WeatherEntry::class], version = 1)
+abstract class AppDatabase : RoomDatabase(){
+    abstract fun weatherDao(): WeatherDao
+
+    companion object {
+        fun instance(context: Context): AppDatabase = Room.databaseBuilder(context.applicationContext, AppDatabase::class.java, "jejakin").build()
+    }
+}
